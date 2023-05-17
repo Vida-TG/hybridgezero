f7ddc3540385967c0a28a83193de61e84e7afd5a
ViDA_TG <victorademiju.t@gmail.com>
2023-05-17 03:04:45 +0100

    Connect wallet

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Connect } from './components/Connect'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Connect />
   )
 }
 
--- src/components/WagmiConfig.js
+++ src/components/WagmiConfig.js
@@ -2,8 +2,12 @@ import { infuraProvider } from 'wagmi/providers/infura'
 import { mainnet, avalanche } from 'wagmi/chains'
 import { createConfig, configureChains, mainnet } from 'wagmi'
 import { publicProvider } from 'wagmi/providers/public'
- 
-export const { chains, publicClient } = configureChains(
+
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
     [mainnet, avalanche],
     [
         infuraProvider({
@@ -13,9 +17,27 @@ export const { chains, publicClient } = configureChains(
         publicProvider()
     ]
 )
- 
+
+
+// Set up wagmi config
 export const config = createConfig({
-  autoConnect: true,
-  publicClient,
-  webSocketPublicClient,
-})
+    autoConnect: true,
+    connectors: [
+      new MetaMaskConnector({ chains }),
+      new CoinbaseWalletConnector({
+        chains,
+        options: {
+          appName: 'HybridgeZero',
+        },
+      }),
+      new InjectedConnector({
+        chains,
+        options: {
+          name: 'Injected',
+          shimDisconnect: true,
+        },
+      }),
+    ],
+    publicClient,
+    webSocketPublicClient,
+  })
