405669b476d47ddeb75e26e545461433e4940d88
ViDA_TG <victorademiju.t@gmail.com>
2023-05-18 13:21:01 +0100

    Connector

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import {
     useAccount,
     useConnect,
@@ -12,7 +13,16 @@ export function Connect() {
     const { data: ensAvatar } = useEnsAvatar({ address })
     const { data: ensName } = useEnsName({ address })
     const { connect, connectors, error, isLoading, pendingConnector } = useConnect()
+    const [ connectorClicked, setConnectorClicked ] = React.useState(false)
     const { disconnect } = useDisconnect()
+
+    function setTheConnectorClicked() {
+      if(connectorClicked){
+        setConnectorClicked(false)
+      } else {
+        setConnectorClicked(true)
+      }
+    }
 
     if (isConnected) {
         return (
@@ -26,25 +36,27 @@ export function Connect() {
     return (
       <>
 
-        <button className='connect-btn'>Connect Wallet</button>
+        <button onClick={setTheConnectorClicked} className='connect-btn'>Connect Wallet</button>
 
-        <div>
-          {connectors.map((connector) => (
-            <button
-              disabled={!connector.ready}
-              key={connector.id}
-              onClick={() => connect({ connector })}
-            >
-              {connector.name}
-              {!connector.ready && ' (unsupported)'}
-              {isLoading &&
-                connector.id === pendingConnector?.id &&
-                ' (connecting)'}
-            </button>
-          ))}
-    
-          {error && <div>{error.message}</div>}
-        </div>
+        { !connectorClicked ? "" :
+          <div className="connect-switch">
+            {connectors.map((connector) => (
+              <button
+                disabled={!connector.ready}
+                key={connector.id}
+                onClick={() => connect({ connector })}
+              >
+                {connector.name}
+                {!connector.ready && ' (unsupported)'}
+                {isLoading &&
+                  connector.id === pendingConnector?.id &&
+                  ' (connecting)'}
+              </button>
+            ))}
+      
+            {error && <div>{error.message}</div>}
+          </div>
+        }
       </>
     )
   }
--- src/components/NetworkSwitcher.js
+++ src/components/NetworkSwitcher.js
@@ -1,26 +1,33 @@
+import { useAccount } from 'wagmi'
 import { useNetwork, useSwitchNetwork } from 'wagmi'
 
 export function NetworkSwitcher() {
+  const { isConnected } = useAccount()
   const { chain } = useNetwork()
   const { chains, error, isLoading, pendingChainId, switchNetwork } =
-    useSwitchNetwork()
+  
+  useSwitchNetwork()
 
-  return (
-    <>
-      {chain && <div>Connected to {chain.name}</div>}
+  if(isConnected) {
+    return (
+      <>
+        <div>
+          {chain && <div>Connected to {chain.name}</div>}
 
-      {chains.map((x) => (
-        <button
-          disabled={!switchNetwork || x.id === chain?.id}
-          key={x.id}
-          onClick={() => switchNetwork?.(x.id)}
-        >
-          {x.name}
-          {isLoading && pendingChainId === x.id && ' (switching)'}
-        </button>
-      ))}
+          {chains.map((x) => (
+            <button
+              disabled={!switchNetwork || x.id === chain?.id}
+              key={x.id}
+              onClick={() => switchNetwork?.(x.id)}
+            >
+              {x.name}
+              {isLoading && pendingChainId === x.id && ' (switching)'}
+            </button>
+          ))}
 
-      <div>{error && error.message}</div>
-    </>
-  )
+          <div>{error && error.message}</div>
+        </div>
+      </>
+    )
+  }
 }
